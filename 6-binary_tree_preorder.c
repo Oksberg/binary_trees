0daaaa91b52adc2eb2497bb3551ffe64c3ce8836
Okyere <oksberg89@gmail.com>
2024-02-28 17:19:12 -0800

    Pre-order traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -8,37 +8,12 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	binary_tree_t *temp;
-
 	if (tree == NULL || func == NULL)
 		return;
 
 	func(tree->n);
-	if (tree->left != NULL)
-	{
-		temp = tree->left;
-		func(temp->n);
 
-		if (tree->left->left != NULL)
-		{
-			temp = tree->left->left;
-			func(temp->n);
-		}
-		temp = tree->left->right;
-		func(temp->n);
-	}
-	if (tree->right != NULL)
-	{
-		temp = tree->right;
-		func(temp->n);
+	binary_tree_preorder(tree->left, func);
 
-		if (tree->right->left != NULL)
-		{
-			temp = tree->right->left;
-			func(temp->n);
-		}
-		temp = tree->right->right;
-		func(temp->n);
-	}
-
+	binary_tree_preorder(tree->right, func);
 }
